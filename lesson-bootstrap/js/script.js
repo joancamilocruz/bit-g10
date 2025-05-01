56fd2f0fb3fa1c909c871cb59ec8193b258cf3c4
Joan Cruz <joancruzcortes@gmail.com>
2025-04-30 22:15:08 -0500

    updates

--- lesson-bootstrap/js/script.js
+++ lesson-bootstrap/js/script.js
@@ -1,1 +1,11 @@
 'use strict'
+const $form = document.getElementById('form');
+
+$form.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    const name = $form.name.value;
+    const message = $form.message.value;
+    console.log(name)
+    console.log(message)
+
+})
